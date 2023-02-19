4ababa524252a32e15db2d7466e3340dcf563874
Ebinabo <olaliebinabo@gmail.com>
2023-02-18 22:11:16 -0800

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-	char abc[26] = "abcdefghijklmnopqrstuvwxyz";
+	char abc[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 24; i++)
 	{
-		if (i != 'q'; && i != 'e')
-			putchar(abc[i]);
+		putchar(abc[i]);
 	}
 	putchar('\n');
 	return (0);
