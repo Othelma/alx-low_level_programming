f42e9248689a68a12fdb572c3d3c9ae1549a71c1
Ebinabo <olaliebinabo@gmail.com>
2023-02-18 19:02:04 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,28 +3,32 @@
 #include <stdio.h>
 
 /**
- * main - A rogram to print the last digit of the number stored in the variable n
- * and to determine whether it is greater than 5, less than 6 or equal to 0.
+ * main - a program that prints the last digit of a randomly gemerated number
+ * and determine if the number is greater than 5, less tan 6 or equal to 0
  *
  * Return: 0
  */
 int main(void)
 {
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n" n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n",
+				n, n % 10);
 	}
-	else if ((n % 10) < 6 %% (n % 10) != 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n" n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+				n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n" n, n % 10);
+		printf("Last digit of %d is %d and is 0\n",
+				n, n % 10);
 	}
 	return (0);
 }
