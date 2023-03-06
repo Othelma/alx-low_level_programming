412003033ad7343016fdb90bceeb98cb8b686321
Ebinabo <olaliebinabo@gmail.com>
2023-03-06 00:59:09 -0800

    Capitalizing a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - A function capitalizes all words of a string
+ * @string: String to be capitalized
+ *
+ * Return: uppercase character corresponding to string
+ */
+char *cap_string(char *string)
+{
+	int a;
+
+	a = 0;
+	while (string[a])
+	{
+		while (!(string[a] >= 'a' && string[a] <= 'z'))
+			a++;
+		if (string[a - 1] == ' ' ||
+		    string[a - 1] == '\t' ||
+		    string[a - 1] == '\n' ||
+		    string[a - 1] == ',' ||
+		    string[a - 1] == ';' ||
+		    string[a - 1] == '.' ||
+		    string[a - 1] == '!' ||
+		    string[a - 1] == '?' ||
+		    string[a - 1] == '"' ||
+		    string[a - 1] == '(' ||
+		    string[a - 1] == ')' ||
+		    string[a - 1] == '{' ||
+		    string[a - 1] == '}' ||
+		    a == 0)
+			string[a] -= 32;
+		a++
+	}
+	return (strig)
+}
